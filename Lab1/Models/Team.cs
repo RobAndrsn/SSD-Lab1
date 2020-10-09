34782d7b35b84d31371d28c595c8b1b73ea3a54d
RobAndrsn <robert-a.anderson1@mohawkcollege.ca>
2020-10-09 17:55:21 -0400

    Add project files.

---
+++ Lab1/Models/Team.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string TeamName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? EstablishedDate { get; set; }
+
+    }
+}
